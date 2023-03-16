74de84f4139d340f1105f745dfe7c3e88ab5712a
nover <109939195+novervr@users.noreply.github.com>
2023-03-15 21:54:20 -0500

    Update PhotonRoom1.cs

--- PhotonRoom1.cs
+++ PhotonRoom1.cs
@@ -6,8 +6,10 @@ namespace PVRServers
 
 public class PhotonRoom1 : MonoBehaviour
 {
+Header["Might not work. Made by NoverVR"]
 public string Handtag;
 public string roomCode;
+//If collider for your hand collides
 void OnTriggerEnter(Collider other)
  {
    if (other.transform.tag == Handtag)
